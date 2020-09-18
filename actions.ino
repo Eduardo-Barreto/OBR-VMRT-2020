56cbc26855d9f65f3a0990cd33bd57005aad6046
Eduardo-Barreto <eduwagzozo@gmail.com>
2020-09-18 19:04:30 -0300

    actions fixed

--- actions.ino
+++ actions.ino
@@ -4,7 +4,19 @@ int KP = 0;
 int gain = 0;
 int error = 0;
 
+int rightDelay = 0;
+int leftDelay = 0;
+
 void seguirLinha(int velocity){
+    targetPower = velocity*1000;
     error = (reflectedLight1 + reflectedLight2) - (reflectedLight3 + reflectedLight4);
-    gain = error * KP
+    gain = error * KP;
+
+    rightDelay = targetPower + gain;
+    leftDelay = targetPower - gain;
+
+    digitalWrite(rightStep, HIGH);
+    digitalWrite(rightStep, LOW);
+    digitalWrite(leftStep, HIGH);
+    digitalWrite(leftStep, LOW);   
 }
