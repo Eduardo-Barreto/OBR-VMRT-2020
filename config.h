e04e84e64a9905eddfce1ec5caa0fff5d57139fb
Eduardo-Barreto <eduwagzozo@gmail.com>
2020-09-18 19:21:17 -0300

    connect light sensors

--- config.h
+++ config.h
@@ -1,5 +1,16 @@
 #include <Arduino.h>
 
+void connectLightSensors(){
+    pinMode(reflecLightFrente, A4);
+    pinMode(reflecLight1, A0);
+    pinMode(reflecLight2, A2);
+    pinMode(reflecLight3, A3);
+    pinMode(reflecLight4, A5);
+    pinMode(reflecLight5, A7);
+    pinMode(leftGreen, A1);
+    pinMode(rightGreen, A6);
+}
+
 void lightRead(){
     int reflecLightFrente = analogRead(A4);
     int reflecLight1 = analogRead(A0);
